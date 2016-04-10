1075f6e9ad052380b63691c380e11c81591041b5
Nuh <hex-tr@hotmail.com>
2016-04-10 23:43:46 +0300

    Category

---
+++ MVCBlog/Areas/Admin/Models/DTO/CategoryVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCBlog.Areas.Admin.Models.DTO
+{
+    public class CategoryVM:BaseVM
+    {
+        [Required(ErrorMessage ="Kategori ismi giriniz")]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+    }
+}
