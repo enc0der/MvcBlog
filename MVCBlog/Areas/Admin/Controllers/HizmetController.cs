027b3dab276a6d798d2c8f84b6f6e833ccb04970
Nuh <hex-tr@hotmail.com>
2016-04-23 15:27:35 +0300

    hizmet listesi eklendi

--- MVCBlog/Areas/Admin/Controllers/HizmetController.cs
+++ MVCBlog/Areas/Admin/Controllers/HizmetController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MVCBlog.Areas.Admin.Models.DTO;
+using MVCBlog.Models.ORM.Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +13,17 @@ namespace MVCBlog.Areas.Admin.Controllers
         
         public ActionResult Index()
         {
-            return View();
+
+            List<HizmetVM> hizmet = db.Hizmets.Where(m => m.IsDeleted == false).OrderByDescending(m => m.ID).Select(m => new HizmetVM()
+            {
+                HizmetAdi = m.HizmetAdi,
+                HizmetIcerik = m.HizmetIcerik,
+                HizmetResim=m.HizmetResim,
+                HizmetSirasi=m.HizmetSirasi,
+                ID=m.ID
+            }).ToList();
+
+            return View(hizmet);
         }
     }
 }
---
+++ MVCBlog/Areas/Admin/Models/DTO/HizmetVM.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCBlog.Areas.Admin.Models.DTO
+{
+    public class HizmetVM : BaseVM
+    {
+        [Required(ErrorMessage ="zorunlu alan")]
+        [Display(Name ="Hizmet Adı")]
+        public string HizmetAdi { get; set; }
+
+        [Required(ErrorMessage = "zorunlu alan")]
+        [Display(Name = "Hizmet İçerik")]
+        public string HizmetIcerik { get; set; }
+
+        [Display(Name = "Hizmet Resmi")]
+        public string HizmetResim { get; set; }
+
+        [Display(Name = "Hizmet sırası")]
+        public int HizmetSirasi { get; set; }
+
+    }
+}
