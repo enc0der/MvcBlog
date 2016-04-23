8ba1d8f0fd5e993697d946b3964c02a3dfeecb96
Nuh <hex-tr@hotmail.com>
2016-04-23 17:09:27 +0300

    hizmet listeleme,ekleme ve silme işlemleri ok

--- MVCBlog/Areas/Admin/Controllers/HizmetController.cs
+++ MVCBlog/Areas/Admin/Controllers/HizmetController.cs
@@ -2,6 +2,7 @@
 using MVCBlog.Models.ORM.Entity;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -11,6 +12,8 @@ namespace MVCBlog.Areas.Admin.Controllers
     public class HizmetController : BaseController
     {
         
+
+        // hizmet listesi anasayfa
         public ActionResult Index()
         {
 
@@ -25,5 +28,69 @@ namespace MVCBlog.Areas.Admin.Controllers
 
             return View(hizmet);
         }
+
+        public ActionResult AddHizmet()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult AddHizmet(HizmetVM model, HttpPostedFileBase HizmetResim)
+        {
+
+            if (ModelState.IsValid)
+            {
+
+                Hizmet hizmet = new Hizmet();
+                hizmet.HizmetAdi = model.HizmetAdi;
+                hizmet.HizmetIcerik = model.HizmetIcerik;
+                hizmet.HizmetSirasi = model.HizmetSirasi;
+                hizmet.HizmetResim = null;
+
+
+                if (HizmetResim != null && HizmetResim.ContentLength > 0)
+                {
+                    string ResimAdi = Guid.NewGuid().ToString().Replace("-", "");
+                    string uzanti = System.IO.Path.GetExtension(Path.GetFileName(HizmetResim.FileName));
+                   string TamYol = Path.Combine(Server.MapPath("~/Content/Img/HizmetResim/" + ResimAdi + uzanti));
+
+                    hizmet.HizmetResim = "~/Content/Img/HizmetResim/" + ResimAdi+ uzanti;
+
+
+                    HizmetResim.SaveAs(TamYol);
+                }
+
+
+                db.Hizmets.Add(hizmet);
+                db.SaveChanges();
+                ViewBag.IslemDurum = 1;
+
+
+                return RedirectToAction("index");
+
+            }else
+            {
+                ViewBag.IslemDurum = 2;
+            }
+
+            return View();
+        }
+
+
+        public JsonResult DeleteHizmet(int id)
+        {
+
+            Hizmet hizmet = db.Hizmets.FirstOrDefault(m=>m.ID==id);
+            hizmet.IsDeleted = true;
+            hizmet.DeleteDate = DateTime.Now;
+            db.SaveChanges();
+
+            return Json("");
+        }
+
+
+
+
     }
 }
