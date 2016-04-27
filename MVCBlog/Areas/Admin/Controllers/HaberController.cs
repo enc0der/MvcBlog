e461fc4b3f266ffe6a86969652b2cdd8144546b7
Nuh <hex-tr@hotmail.com>
2016-04-27 19:17:04 +0300

    haber güncelleme ok.

--- MVCBlog/Areas/Admin/Controllers/HaberController.cs
+++ MVCBlog/Areas/Admin/Controllers/HaberController.cs
@@ -17,10 +17,10 @@ namespace MVCBlog.Areas.Admin.Controllers
             List<HaberVM> model = db.Habers.Where(m => m.IsDeleted == false).OrderByDescending(m => m.ID).Select(m => new HaberVM()
             {
 
-                HaberBaslik =m.HaberBaslik,
+                HaberBaslik = m.HaberBaslik,
                 HaberIcerik = m.HaberIcerik,
-                Resim=m.HaberIcerik,
-                ID=m.ID
+                Resim = m.HaberIcerik,
+                ID = m.ID
 
             }).ToList();
 
@@ -29,7 +29,8 @@ namespace MVCBlog.Areas.Admin.Controllers
 
         public ActionResult AddHaber()
         {
-            return View();
+            HaberVM model = new HaberVM();
+            return View(model);
         }
 
         [HttpPost]
@@ -37,14 +38,14 @@ namespace MVCBlog.Areas.Admin.Controllers
         public ActionResult AddHaber(HaberVM model, HttpPostedFileBase HaberResim)
         {
 
-            if(ModelState.IsValid)
+            if (ModelState.IsValid)
             {
                 Haber haber = new Haber();
                 haber.HaberBaslik = model.HaberBaslik;
                 haber.HaberIcerik = model.HaberIcerik;
                 haber.Resim = null;
 
-                if(HaberResim != null && HaberResim.ContentLength>0)
+                if (HaberResim != null && HaberResim.ContentLength > 0)
                 {
                     string ResimAdi = Guid.NewGuid().ToString().Replace("-", "");
                     string uzanti = System.IO.Path.GetExtension(Path.GetFileName(HaberResim.FileName));
@@ -71,7 +72,7 @@ namespace MVCBlog.Areas.Admin.Controllers
 
         public ActionResult UpdateHaber(int id)
         {
-            Haber haber = db.Habers.FirstOrDefault(m=>m.ID==id);
+            Haber haber = db.Habers.FirstOrDefault(m => m.ID == id);
 
             HaberVM model = new HaberVM();
             model.HaberBaslik = haber.HaberBaslik;
@@ -82,10 +83,48 @@ namespace MVCBlog.Areas.Admin.Controllers
             return View(model);
         }
 
+
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult UpdateHaber(HaberVM model, HttpPostedFileBase HaberResim)
+        {
+            if (ModelState.IsValid)
+            {
+                Haber haber = db.Habers.FirstOrDefault(m=>m.ID==model.ID);
+                haber.HaberBaslik = model.HaberBaslik;
+                haber.HaberIcerik = model.HaberIcerik;
+                haber.Resim = model.Resim;
+
+                if (HaberResim != null && HaberResim.ContentLength > 0)
+                {
+                    string ResimAdi = Guid.NewGuid().ToString().Replace("-", "");
+                    string uzanti = System.IO.Path.GetExtension(Path.GetFileName(HaberResim.FileName));
+                    string TamYol = Path.Combine(Server.MapPath("/Content/Img/HaberResim/" + ResimAdi + uzanti));
+
+                    haber.Resim = "/Content/Img/HaberResim/" + ResimAdi + uzanti;
+
+
+                    HaberResim.SaveAs(TamYol);
+                }
+
+                db.SaveChanges();
+                ViewBag.IslemDurum = 1;
+                return RedirectToAction("index");
+               
+            }
+            else
+            {
+                ViewBag.IslemDurum = 2;
+                return View(model);
+            }
+
+        }
+
+
         public JsonResult DeleteHaber(int id)
         {
 
-            Haber model = db.Habers.FirstOrDefault(m=>m.ID==id);
+            Haber model = db.Habers.FirstOrDefault(m => m.ID == id);
             model.IsDeleted = true;
             model.DeleteDate = DateTime.Now;
 
@@ -93,7 +132,5 @@ namespace MVCBlog.Areas.Admin.Controllers
 
             return Json("");
         }
-
-
     }
 }
