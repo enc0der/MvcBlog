59db458787c9f34bad502f2dd9b7522ff1986167
Nuh ÇOLAKKADIOĞLU <hex-tr@hotmail.com>
2016-05-27 10:13:56 +0300

    Update BlogPostVM.cs

--- MVCBlog/Areas/Admin/Models/DTO/BlogPostVM.cs
+++ MVCBlog/Areas/Admin/Models/DTO/BlogPostVM.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+//
 namespace MVCBlog.Areas.Admin.Models.DTO
 {
     public class BlogPostVM:BaseVM
